f80dabc63f793dd7b29b718b816ab4841ba823a1
JensFillee <r0800028@student.thomasmore.be>
2021-10-12 14:29:26 +0200

    initial commit

---
+++ src/main/java/fact/it/apt_themeparkservice/AptThemeParkServiceApplication.java
@@ -0,0 +1,13 @@
+package fact.it.apt_themeparkservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AptThemeParkServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AptThemeParkServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fact/it/apt_themeparkservice/AptThemeParkServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fact.it.apt_themeparkservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AptThemeParkServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
