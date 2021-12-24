4ab5fa7f7bebdc9a6b2baf610c6a5ab9dbd763c5
JensFillee <r0800028@student.thomasmore.be>
2021-12-24 11:27:15 +0100

    added /api prefix

--- src/main/java/fact/it/apt_themeparkservice/controller/ThemeparkCotroller.java
+++ src/main/java/fact/it/apt_themeparkservice/controller/ThemeparkCotroller.java
@@ -5,12 +5,14 @@ import fact.it.apt_themeparkservice.repository.ThemeparkRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class ThemeparkCotroller {
 
     @Autowired
